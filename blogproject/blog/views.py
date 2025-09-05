9f175c19c1ea2dbe1154e661c651f504d327df5d
Asim Patrick <asimokon@yahoo.com>
2025-09-05 19:56:04 +0100

    first working API

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def home(request):
+    data = {
+        "message": "Hello, Django",
+        "status": "success"
+    }
+    
+    return JsonResponse(data)
