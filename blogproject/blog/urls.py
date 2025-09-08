85263da6b54b6f6cc0a20131155dc50d99379c14
Asim <asimokon@yahoo.com>
2025-09-08 23:11:56 +0100

    added serillizers

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('post/', views.makePost, name='post'),
-    path('posts/', views.getPosts, name='posts'),
+    path('posts/', views.getPosts, name='posts'), 
 
 ]
